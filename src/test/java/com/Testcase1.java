80bc77ec1e2e93ef2f165566288364c8e8256608
kusumsuhas <ravindra.v3@gmail.com>
2017-12-19 11:38:10 +0530

    updated

--- src/test/java/com/Testcase1.java
+++ src/test/java/com/Testcase1.java
@@ -1,9 +1,14 @@
 package com;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class Testcase1 {
 
+	public WebDriver driver;
+	
 	@Test
 	public void TestMethod1() {
 		System.out.println("Test Method is Passed");
@@ -14,4 +19,17 @@ public class Testcase1 {
 		System.out.println("Test Method 2 is Updated;");
 	}	
 	
+	@Test
+	public void OpenBrowser() {
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Trainings\\Selenium\\Drivers\\chromedriver_win32_V2.33\\chromedriver.exe");
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("disbale-infobars");
+		driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("http://mvnrepository.com/");
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
 }
